a0fc476cee2d2a4fe869c5694addef8217e656f5
stefanny233 <stefanny24si@mahasiswa.pcr.ac.id>
2026-05-01 16:05:25 +0700

    PERTEMUAN 6: Tugas mandiri untuk kampus

--- app/src/main/java/com/example/paniapps/MainActivity.kt
+++ app/src/main/java/com/example/paniapps/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.paniapps
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import com.example.paniapps.databinding.ActivityMainBinding
 import com.example.paniapps.pertemuan_4.FourthActivity
@@ -16,9 +17,32 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val sharedPref = getSharedPreferences("user_pref", MODE_PRIVATE)
+
         binding.btnToFourth.setOnClickListener {
             val intent = Intent(this, FourthActivity::class.java)
             startActivity(intent)
+        }
+
+        binding.btnLogout.setOnClickListener {
+
+            AlertDialog.Builder(this)
+                .setTitle("Logout")
+                .setMessage("Yakin ingin logout?")
+                .setPositiveButton("Ya") { dialog, _ ->
+
+                    val editor = sharedPref.edit()
+                    editor.clear()
+                    editor.apply()
+
+                    dialog.dismiss()
+
+                    startActivity(Intent(this, AuthActivity::class.java))
+                    finish()
+                }
+
+                .setNegativeButton("Tidak", null)
+                .show()
         }
     }
 }
